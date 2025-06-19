10b85bda5b4c503448138e93ec234f1c9153c3b5
harsh-coder-desgin <harshpatel51057@gmail.com>
2025-06-20 00:37:02 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,27 +1,20 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+
 const app = express()
 
-
 app.use(cors({
-    origin:"https://aurcode.vercel.app",
-    credentials :true
+  origin: "https://aurcode.vercel.app",
+  credentials: true
 }))
-// app.use(cors({
-//   origin: "http://localhost:3000",
-//   credentials: true
-// }));
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-
 import userRouter from "./routes/user.routes.js"
-
-app.use("/api/v1/users",userRouter)
-
+app.use("/api/v1/users", userRouter)
 
 export { app }
